18a3fbb594c8fd72219ef78199b221b5d8ab4f25
potter28 <potter28@purdue.edu>
2019-02-08 18:06:42 -0500

    Adding some visualizations

--- 2_5/Live.py
+++ 2_5/Live.py
@@ -1,5 +1,3 @@
-# purdueieeecsociety.slack.com
-# discussion thread -> data
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,16 +13,37 @@ BATCH_SIZE = 70
 def getData(directory, filename):
 	data = read_csv(directory + "/" + filename)
 
+	#	Drop non-numeric data
 	data.drop(labels=['date','label'], axis=1, inplace=True)
 
+	#	Normalize to [0,1]
 	values = data.values/data.values.max(axis=0)
 
+	print(values.shape)
+	pp(values[0:3,:])
+
+	#	Append the output vector
 	inputs = values[:-1]
 	outputs = values[1:,3].reshape(values[1:,3].shape[0],1)
 	values = np.concatenate((inputs, outputs), 1)
 
+	#	Test/Train split
 	train = values[:int(values.shape[0] * TEST_PORTION)]
 	test = values[int(values.shape[0] * TEST_PORTION):]
+
+	print((train.shape, test.shape))
+
+	x = np.array(list(range(values.shape[0])))
+	plt.plot(x, values[:,3])
+	plt.show()
+	plt.clf()
+
+	x_train = x[:int(x.shape[0] * TEST_PORTION)]
+	x_test = x[int(x.shape[0] * TEST_PORTION):]
+	plt.plot(x_train, train[:,3])
+	plt.plot(x_test, test[:,3])
+	plt.show()
+	plt.clf()
 
 	return (train, test)
 
@@ -34,11 +53,18 @@ def getModel(train_x, train_y, test_x, test_y):
 	model.add(Dense(1))
 	model.compile(loss='mse', optimizer='adam')
 
+	#	Train
 	history = model.fit(train_x, train_y,
 		epochs=NUM_EPOCHS,
 		batch_size=BATCH_SIZE,
 		validation_data=(test_x, test_y),
 		shuffle=False)
+
+	plt.plot(history.history['loss'], label='train')
+	plt.plot(history.history['val_loss'], label='test')
+	plt.legend()
+	plt.show()
+	plt.clf()
 
 	return model
 
@@ -48,9 +74,9 @@ def stockPolicy(market):
 if __name__ == '__main__':
 	train, test = getData("data", "AAPL.csv")
 
+	#	Reshape into format expected by LSTMS
 	train_x, train_y = train[:,:-1], train[:,-1]
 	test_x, test_y = test[:,:-1], test[:,-1]
-
 	train_x = train_x.reshape(train_x.shape[0], 1, train_x.shape[1])
 	test_x = test_x.reshape(test_x.shape[0], 1, test_x.shape[1])
 
@@ -58,9 +84,17 @@ if __name__ == '__main__':
 
 	y_hat = model.predict(test_x)
 
-	real = test_x[1:,0,3]
-	naive = test_x[:-1,0,3]
+	#	Perform some analytical analysis
+	real = test_y[1:]
+	naive = test_y[:-1]	#	Assume t(i+1) = t(i)
 	predicted = y_hat[1:].reshape(y_hat[1:].shape[0])
+
+	plt.plot(real, label='real')
+	plt.plot(naive, label='naive')
+	plt.plot(predicted, label='predicted')
+	plt.legend()
+	plt.show()
+	plt.clf()
 
 	placedBets = stockPolicy(predicted)
 	optimalBets = stockPolicy(real)
